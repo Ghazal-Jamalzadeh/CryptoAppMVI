6fa5d8df09074b732ff41fd12a41f83b15fa2200
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-02 16:56:17 +0430

    #434 MainFragment - CoinsList : handle states

--- app/src/main/java/com/jmzd/ghazal/cryptoappmvi/ui/main/MainFragment.kt
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/ui/main/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -12,6 +13,8 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.jmzd.ghazal.cryptoappmvi.R
 import com.jmzd.ghazal.cryptoappmvi.databinding.FragmentMainBinding
 import com.jmzd.ghazal.cryptoappmvi.utils.base.BaseFragment
+import com.jmzd.ghazal.cryptoappmvi.utils.base.BaseState
+import com.jmzd.ghazal.cryptoappmvi.utils.showSnackBar
 import com.jmzd.ghazal.cryptoappmvi.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -31,14 +34,38 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
 
         //call api
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.CREATED){
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
                 if (isNetworkAvailable) {
                     viewModel.intentChannel.send(MainIntent.GetCoinsList)
                 }
             }
+        }
 
-            }
+        handleStates()
 
     }
+
+    private fun handleStates() {
+        binding.apply {
+            viewLifecycleOwner.lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.CREATED) {
+                    viewModel.state.collect { state ->
+                        when (state) {
+                            is BaseState.Idle -> {}
+                            is BaseState.Loading -> coinsMarketLoading.isVisible = true
+                            is BaseState.Error -> {
+                                state.error?.let { root.showSnackBar(it) }
+                            }
+
+                            is BaseState.Main.LoadCoinsList ->{}// initCoinsSpinner(state.coinsList)
+                            else -> {}
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
 
 }
