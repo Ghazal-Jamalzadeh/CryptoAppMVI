8893105bacdfd964108f4d8f240bd76c807ad335
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-02 14:02:48 +0430

    #432 MainFragment - CoinsList : api services

--- app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/network/ApiServices.kt
@@ -1,4 +1,10 @@
 package com.jmzd.ghazal.cryptoappmvi.data.network
 
+import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseCoinsList
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiServices {
+    @GET("coins/list")
+    suspend fun getCoinsList() : Response<ResponseCoinsList>
 }
