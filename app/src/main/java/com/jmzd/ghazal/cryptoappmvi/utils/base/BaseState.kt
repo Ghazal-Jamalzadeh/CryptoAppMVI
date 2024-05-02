e76e8f1813783d0822bd17d565692e6b4652e3c0
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-02 17:29:05 +0430

    #435 MainFragment - SupportedCurrencies : state

--- app/src/main/java/com/jmzd/ghazal/cryptoappmvi/utils/base/BaseState.kt
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/utils/base/BaseState.kt
@@ -1,6 +1,7 @@
 package com.jmzd.ghazal.cryptoappmvi.utils.base
 
 import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseCoinsList
+import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseSupportedCurrencies
 
 sealed class BaseState ( val error : String? = null) {
     data object Idle : BaseState()
@@ -9,5 +10,6 @@ sealed class BaseState ( val error : String? = null) {
 
     sealed class Main : BaseState() {
         data class LoadCoinsList(val coinsList: ResponseCoinsList) : Main()
+        data class LoadSupportedCurrenciesList(val coinsList: ResponseSupportedCurrencies) : Main()
     }
 }
