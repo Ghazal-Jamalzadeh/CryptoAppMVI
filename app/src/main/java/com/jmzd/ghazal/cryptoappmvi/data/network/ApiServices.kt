76256432e94c36747b4035ef0b6ed3e633445be9
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-02 17:26:33 +0430

    #435 MainFragment - SupportedCurrencies : api services

--- app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/network/ApiServices.kt
@@ -1,10 +1,16 @@
 package com.jmzd.ghazal.cryptoappmvi.data.network
 
 import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseCoinsList
+import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseSupportedCurrencies
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiServices {
+
     @GET("coins/list")
     suspend fun getCoinsList() : Response<ResponseCoinsList>
+
+    @GET("simple/supported_vs_currencies")
+    suspend fun getSupportedCurrencies() : Response<ResponseSupportedCurrencies>
+
 }
