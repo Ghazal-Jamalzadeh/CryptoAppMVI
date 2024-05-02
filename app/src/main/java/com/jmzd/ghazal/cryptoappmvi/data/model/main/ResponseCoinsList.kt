43ca95c9177ff786300fe2ebf824902b0d5b241f
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-02 14:00:31 +0430

    #432 MainFragment - CoinsList : model

---
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/model/main/ResponseCoinsList.kt
@@ -0,0 +1,15 @@
+package com.jmzd.ghazal.cryptoappmvi.data.model.main
+
+
+import com.google.gson.annotations.SerializedName
+
+class ResponseCoinsList : ArrayList<ResponseCoinsList.ResponseCoinsListItem>(){
+    data class ResponseCoinsListItem(
+        @SerializedName("id")
+        val id: String?, // 01coin
+        @SerializedName("name")
+        val name: String?, // 01coin
+        @SerializedName("symbol")
+        val symbol: String? // zoc
+    )
+}
