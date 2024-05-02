af3a6c1a5691ec7ed1960add97103941cf9e05e4
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-02 13:23:49 +0430

    #432 MainActivity

--- app/src/main/java/com/jmzd/ghazal/cryptoappmvi/ui/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/ui/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.jmzd.ghazal.cryptoappmvi.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
 import com.jmzd.ghazal.cryptoappmvi.R
+import com.jmzd.ghazal.cryptoappmvi.databinding.ActivityMainBinding
+import com.jmzd.ghazal.cryptoappmvi.utils.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BaseActivity() {
+    //Binding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        //Status bar Transparent
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = Color.TRANSPARENT
+        }    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
