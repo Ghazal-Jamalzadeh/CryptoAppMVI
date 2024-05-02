a745bcc3d7b1ad0f3cb5e4d011748573e0933889
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-02 12:39:59 +0430

    #431 project structure : manifest + application class

---
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/network/ApiServices.kt
@@ -0,0 +1,4 @@
+package com.jmzd.ghazal.cryptoappmvi.data.network
+
+interface ApiServices {
+}
--- app/src/main/java/com/jmzd/ghazal/cryptoappmvi/ui/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jmzd.ghazal.cryptoappmvi
+package com.jmzd.ghazal.cryptoappmvi.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jmzd.ghazal.cryptoappmvi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
