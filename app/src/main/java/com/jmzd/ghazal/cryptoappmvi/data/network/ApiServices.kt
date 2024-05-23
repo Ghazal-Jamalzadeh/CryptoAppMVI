40ea1e0dcfa8f129e84c86bcddf5c289b0d683a5
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-23 12:44:28 +0430

    #440 DetailFragment : api services

--- app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/network/ApiServices.kt
+++ app/src/main/java/com/jmzd/ghazal/cryptoappmvi/data/network/ApiServices.kt
@@ -1,10 +1,12 @@
 package com.jmzd.ghazal.cryptoappmvi.data.network
 
+import com.jmzd.ghazal.cryptoappmvi.data.model.detail.ResponseDetail
 import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseCoinsList
 import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseCoinsMarket
 import com.jmzd.ghazal.cryptoappmvi.data.model.main.ResponseSupportedCurrencies
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiServices {
@@ -25,5 +27,9 @@ interface ApiServices {
                                 @Query("sparkline") sparkLine : Boolean  ,
                                 @Query("page") page : Int)
     : Response<ResponseCoinsMarket>
+
+    @GET("coins/{id}")
+    suspend fun getCoinDetail(@Path("id") id: String, @Query("sparkline") sparkline: Boolean): Response<ResponseDetail>
+
 
 }
